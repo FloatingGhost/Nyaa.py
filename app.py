5c7ab0a13d91d83316fc1719dcc853823dba4e1c
Hannah Ward <Hannah.ward9001@gmail.com>
2017-05-04 22:09:17 +0100

    Add basic SQL query

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import sqlite3
 
 app = Flask(__name__)
+database = sqlite3.connect("db.sqlite")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    try:
+        pageNo = int(request.args.get("page", 0))
+    except:
+        pageNo = 0
+
+    cursor = database.cursor()
+    ret = cursor.execute("""
+        SELECT 
+            name
+        FROM 
+            nyaa
+        LIMIT 
+            50
+        OFFSET 
+            (?)
+    """, [pageNo * 50])
+    return render_template("index.html", rows=list(ret))
 
 if __name__ == "__main__":
     app.run()
